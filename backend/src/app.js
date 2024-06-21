16e7c2462035b5ce0304e2d7953cfa1423fab56b
srujanGowda08 <srujangamer08@gmail.com>
2024-06-21 10:00:48 +0530

    usn

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,28 +1,26 @@
-import express from "express"
-import cors from "cors"
-import cookieParser from "cookie-parser"
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-const app = express()
+const app = express();
 
-app.use(cors({ 
-    origin: process.env.CORS_ORIGIN, 
-    credentials: true 
-}))
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
 
-app.use(express.json({ limit:"16kb" }))
-app.use(express.urlencoded({ extended: true, limit: "16kb" }))
-app.use(express.static("public"))
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
 
-app.use(cookieParser())
+app.use(cookieParser());
 
+import userRouter from "./routes/user.route.js";
+import adminRouter from "./routes/admin.route.js";
 
-import userRouter from "./routes/user.route.js"
-import adminRouter from "./routes/admin.route.js"
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/admins", adminRouter);
 
-app.use("/api/v1/users", userRouter)
-app.use("/api/v1/admins", adminRouter)
-
-export { app }
-
-
-
+export { app };
